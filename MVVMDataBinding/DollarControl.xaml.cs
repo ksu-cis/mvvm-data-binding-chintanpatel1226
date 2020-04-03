cdf53dbcc227d9222bc9d2f3abfecd3bc6672d6e
Chintan Patel <chintanpatel1226@ksu.edu>
2020-04-03 12:38:28 -0500

    done

--- MVVMDataBinding/DollarControl.xaml.cs
+++ MVVMDataBinding/DollarControl.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CashRegister;
 
 namespace MVVMDataBinding
 {
@@ -18,9 +19,51 @@ namespace MVVMDataBinding
     /// </summary>
     public partial class DollarControl : UserControl
     {
+        /// <summary>
+        /// The DependencyProperty for the DenominationProperty
+        /// </summary>
+        public static readonly DependencyProperty DenominationProperty = DependencyProperty.Register(
+            "Denomination",
+            typeof(Bills),
+            typeof(DollarControl),
+            new PropertyMetadata(Bills.One)
+            );
+
+        /// <summary>
+        /// The Denomination this control displays and modifies
+        /// </summary>
+        public Bills Denomination
+        {
+            get => (Bills)GetValue(DenominationProperty);
+            set => SetValue(DenominationProperty, value);
+        }
+
+        public static readonly DependencyProperty QuantityProperty = DependencyProperty.Register(
+            "Quantity",
+            typeof(int),
+            typeof(DollarControl),
+            new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)
+            );
+
+        public int Quantity
+        {
+            get => (int)GetValue(QuantityProperty);
+            set => SetValue(QuantityProperty, value);
+        }
+
         public DollarControl()
         {
             InitializeComponent();
+        }
+
+        public void OnIncreaseClicked(object sender, RoutedEventArgs e)
+        {
+            Quantity++;
+        }
+
+        public void OnDecreaseClicked(object sender, RoutedEventArgs e)
+        {
+            Quantity--;
         }
     }
 }
